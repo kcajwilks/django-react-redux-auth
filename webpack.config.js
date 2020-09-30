5514260d312c86c604a7ff705555ce7ade179f09
kcajwilks <kcaj.wilks@gmail.com>
2020-09-30 23:30:05 +0100

    added loaders for argon template

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,30 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              import: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
 };
